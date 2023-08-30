4595b93b910f4c1fc02ee8a28e62c5bd1ab767f1
Emiliano Mendoza <cemh.fi@comunidad.unam.mx>
2023-08-30 00:58:02 -0600

    Mas ejemplos

---
+++ ejemplowhile.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+######################################################################
+# while [ CONDICION ]; do
+#     instrucciones
+# done
+######################################################################
+#
+# CONT=0
+# while [ $CONT -lt 5 ]; do
+#     echo El contador es $CONT
+#     $CONT=$(($CONT+1))
+# done
